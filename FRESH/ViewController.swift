7dc2860c699754fc03d5ffafe3b969aeabe4e607
Sean <sean@imacs-imac-2.local>
2018-10-15 20:12:59 +1000

    Initial Commit

---
+++ FRESH/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FRESH
+//
+//  Created by Sean  on 15/10/18.
+//  Copyright © 2018 Sean . All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
